c7a73bf05df96fdbcf4db81e48c30adafcd5472c
Lauren <lpappone@gmail.com>
2014-08-25 17:39:31 -0700

    sort and merge csv files

--- main.js
+++ main.js
@@ -29,14 +29,37 @@ var processFileList = function(filename) {
   return fileRange;
 };
 
+var csvMerge = function(array) {
+  if (array[0].length === 0) {
+    array = array.shift();
+  }
+  var stack = [[parseInt(array[0][0]), parseInt(array[0][1])]];
+  for (var i = 1; i < array.length; i++) {
+    if (array[i].length !== 0) {
+      var prevVal = stack[stack.length-1];
+      if (+array[i][0] > +prevVal[1]) {
+        stack.push([parseInt(array[i][0]), parseInt(array[i][1])]);
+      } else {
+        var newVal = [parseInt(prevVal[0]), parseInt(array[i][1])];
+        stack.pop();
+        stack.push(newVal);
+      }
+    }
+  }
+  return stack; 
+}
+
 var processCsvList = function(csvLocation){
   csv.fromPath(csvLocation).on('record', function(data) {
+    if (data.length !== 0) {
       csvData.push(data);
+    }
     }).on('end', function() {
+      console.log('csvdata' , csvData);
       csvSorted = csvData.sort(function(a, b) {
         if (a[0] === b[0]) {
-          x = a[1];
-          y = b[1];
+          x = parseInt(a[1]);
+          y = parseInt(b[1]);
           if (x > y) {
             return 1;
           } else if (x < y) {
@@ -45,9 +68,11 @@ var processCsvList = function(csvLocation){
             return 0;
           }
         }
-        return a[0] - b[0]; 
+        return parseInt(a[0]) - parseInt(b[0]); 
       });
       console.log(csvSorted);
+      finalCsv = csvMerge(csvSorted);
+      console.log(finalCsv);
     });
 };
 
